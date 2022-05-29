1693988492b8824e3ede2621a008ee056ce58c63
Denis <dgeorgiev436@gmail.com>
2022-05-29 15:58:33 +0000

    Preparing for deploying

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db")
+const path = require("path")
 
 
 // Connect to Database
@@ -9,9 +10,6 @@ connectDB();
 // Init middleware
 app.use(express.json({extended: false}))
 
-app.get("/", (req,res) => {
-	res.send("HOME PAGE")
-})
 
 // Define routes
 
@@ -21,6 +19,17 @@ app.use("/api/profile", require("./routes/api/profile"))
 app.use("/api/auth", require("./routes/api/auth"))
 
 
+// Serve static assets in production 
+
+if(process.env.NODE_ENV === "production"){
+// 	Set static folder
+	app.use(express.static('client/build'));
+	
+	app.get("*", (req,res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+	})
+	
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
